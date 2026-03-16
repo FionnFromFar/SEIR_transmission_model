0938eb2fe62f273bb0647fbd497cb2449f7edb62
Fionn Doheny <fionndoheny73@gmail.com>
2026-03-16 12:57:15 +0000

    In this commit i have defined the intial population conditions and the model parameters to verify the function works

--- seir_ode.py
+++ seir_ode.py
@@ -11,3 +11,20 @@ def seir(y, t, beta, sigma, gamma): #defining the SEIR equations in a function
     drdt = gamma * i #equation for recovered
 
     return [dsdt, dedt, didt, drdt] 
+
+#setting up model parameters (from assesment brief)
+beta = 1.0 #infection rate
+sigma = 1.0 #incubation rate
+gamma = 0.1 #recovery rate
+
+#setting up basic initial conditions for first run (as percentage of population)
+s0 = 0.99 #initial susceptible
+e0 = 0.01 #initial exposed
+i0 = 0.0 #initial infected
+r0 = 0.0 # intial recovered
+
+#setting up initial condisitons in the list y for the function
+y0 = [s0, e0, i0, r0]
+
+#creating a list to represent 100 days as per instructions
+t = np.linspace(0, 100, 100)
