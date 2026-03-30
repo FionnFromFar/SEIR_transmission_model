20bdf5ad666e688421de94016b8d0bd351e70b99
Fionn Doheny <fionndoheny73@gmail.com>
2026-03-30 11:58:04 +0100

    I have initialised the basics for my c++ environment which im going to be using for this part. I have written up the Agent header file including the agent class stucture

---
+++ part2/cpp_source/Agent.hpp
@@ -0,0 +1,33 @@
+#ifndef AGENT_HPP //header guard to prevent being called twice
+#define AGENT_HPP
+
+// defining the SEIR states
+enum class State { Susceptible, Exposed, Infected, Recovered }; //using enum class to prevent leaking into global scope
+
+class Agent {
+private:
+    // making it private so that only the agent can change its own values (this was in my feedback for test2)
+    double x, y; //current positions in the box
+    double vx, vy; //current velocities
+    State status; //current health status (S, E, I or R)
+
+public:
+    //Constructor function to run and intialise a new agent with position, velocity and state
+    Agent(double x0, double y0, double vx0, double vy0, State s0);
+
+    //getter functions to allow reading of the data without changing it
+    double getX() const { return x; }
+    double getY() const { return y; } //return both the x and y coords
+    State getStatus() const { return status; } //returns health status
+
+    //setter function whcih changes an agents health status if need be (ie becoming infected from susceptible when in contacted with an infected agent)
+    void setStatus(State s) { status = s; }
+
+    //movement logic used for updating position and also handling bouncing off the walls of the box instead of moving through it
+    void move (double dt, double L);
+    //dt is the time step
+    //L is the length and width of the box
+
+};
+
+#endif //eding the header guard
