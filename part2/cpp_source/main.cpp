e199fee4da76ced91d0b243b5729d46eeaf327c3
Fionn <fionndoheny73@gmail.com>
2026-03-31 12:26:25 +0100

    I added in the main infection loop process to main.cpp This loop checks if the following two conditions are met. 1:two agents are close enough to eachother (distance < infection radius) 2:one of the agents is susceptible and the other is infected. If these two conditions are met then the susceptible agents Status is changed to exposed. At the moment, only patient 0 is infecting people because there is no clock to change exposed agents to infected ones. This is what im going to add next

--- part2/cpp_source/main.cpp
+++ part2/cpp_source/main.cpp
@@ -28,8 +28,13 @@ int main() {
         double vx = dist_vel(gen);
         double vy = dist_vel(gen);
 
-        //making sure that initially, everyone is susceptible
+        //making sure that initially, everyone is susceptible (correction!)
         State s = State::Susceptible;
+
+        //mannually adding one initial infected agent
+        if (i == 0) {
+            s =State::Infected;
+        }
     
         //creating a new agent with position and velocity and add it to the simulation
         agents.push_back(Agent(x, y, vx, vy, s));
@@ -47,9 +52,39 @@ int main() {
             //calling the propagation step defined in the header file
             //which updates x and y and behaves well at the boundaries
             a.move(dt, L);
-            
-        }
+        
+        //interaction step
+        double infection_radius = 2.0; //distance required for disease to spead
+        double transmission_prob = 0.5; //50% chance of an interaction leading to transmission
+
+        for (size_t i = 0; i < agents.size(); ++i) {
+            for (size_t j = i + 1; j < agents.size(); ++j) {
+
+                //checking if transmission is possible (interaction between infected and susceptible)
+                if ((agents[i].getStatus() == State::Infected && agents[j].getStatus() == State::Susceptible) ||
+                    (agents[j].getStatus() == State::Infected && agents[i].getStatus() == State::Susceptible)) {
+
+                        //checking if distance is within range
+                        if (agents[i].distanceTo(agents[j]) < infection_radius) {
+
+                            //generate a random probability and comparing to 0.5 (flipping a coin basically)
+                            std::uniform_real_distribution<double> roll(0.0, 1.0);
+                            if (roll(gen) < transmission_prob) {
+                                //small line to check if transmission mechanism is working
+                                std::cout << "Agent " << j << " was exposed by Agent " << i << "!" << std::endl;
+                                //change state from susceptible to exposed
+                                if (agents[i].getStatus() == State::Susceptible)
+                                    agents[i].setStatus(State::Exposed);
+                                else
+                                    agents[j].setStatus(State::Exposed);
+                            }
+                        }
+                    }
+                }
+            }
+        }        
     }
+
 
     std::cout << "Initialised " << agents.size() << " agents with dt = " << dt << std::endl;
 
