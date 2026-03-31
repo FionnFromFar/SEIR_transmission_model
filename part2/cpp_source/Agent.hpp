2865981377c5fd7c0e68427e7da869ad0ec7aef4
Fionn <fionndoheny73@gmail.com>
2026-03-31 15:22:22 +0100

    Started changing the system to fit the discrete lattice simulation, I have edited the double x and y positions to be the discrete row and column values. Next i will edit the Agent.cpp

--- part2/cpp_source/Agent.hpp
+++ part2/cpp_source/Agent.hpp
@@ -1,45 +1,42 @@
+//LATICE BRANCH
 #ifndef AGENT_HPP //header guard to prevent being called twice
 #define AGENT_HPP
 
-// defining the SEIR states
-enum class State { Susceptible, Exposed, Infected, Recovered }; //using enum class to prevent leaking into global scope
+#include <vector>
+#include <random>
+
+// defining the SEIR states as per assessment brief (0 is just an empty position)
+enum class State { Susceptible = 1, Exposed = 2, Infected = 3, Recovered = 4 }; //using enum class to prevent leaking into global scope
 
 class Agent {
 private:
     // making it private so that only the agent can change its own values (this was in my feedback for test2)
-    double x, y; //current positions in the box
-    double vx, vy; //current velocities
+    int r, c; //changed from doubles of x and y position to row and column for the lattice
     State status; //current health status (S, E, I or R)
-    double timeInState; //tracks the time spent in any state
+    double timeInState; //tracks the time spent in any state (these two are the same as im gonna use them in the same way)
 
 public:
-    //Constructor function to run and intialise a new agent with position, velocity and state
-    Agent(double x0, double y0, double vx0, double vy0, State s0);
+    //Constructor function to run and intialise a new agent with row, column and state
+    Agent(int r0, int c0, State s0);
 
-    //function to calculate distance between agents
-    double distanceTo(const Agent& other) const;
+    //move function checks adjascent positions are empty to determine which can be moved into
+    void move(std::vector<std::vector<int>>& grid, int L, std::mt19937& gen);
+
+    //new function to check for infected neighbors
+    bool hasInfectedNeighbor(const std::vector<std::vector<int>>& grid, int L)
 
     //getter functions to allow reading of the data without changing it
-    double getX() const { return x; }
-    double getY() const { return y; } //return both the x and y coords
-    State getStatus() const { return status; } //returns health status
+    int getRow() const { return r; }
+    int getCol() const { return c; } //return both the x and y coords
+    State getStatus() const { return status; }//returns health status
 
     //setter function whcih changes an agents health status if need be (ie becoming infected from susceptible when in contacted with an infected agent)
     void setStatus(State s) { status = s; }
 
-    //movement logic used for updating position and also handling bouncing off the walls of the box instead of moving through it
-    void move (double dt, double L);
-    //dt is the time step
-    //L is the length and width of the box
-
-    //getting the state so that it can be changed if neccecary after infected/susceptible interatction
-    State getState() const {return status; }
-    void setState(State newState) { status = newState; }
-
+    //timer functions remain the same
     void updateTime(double dt) {timeInState += dt; }
     void resetTime() {timeInState = 0.0; }
     double getTime() const {return timeInState; }
-
 };
 
 #endif //eding the header guard
